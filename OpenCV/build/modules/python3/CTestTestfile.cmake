2340ce3e4f9964174f87c9b63e56792eb41afe44
Maxim Liefaard <m.liefaard@student.tudelft.nl>
2018-05-21 17:23:26 +0200

    Removed pre-built OpenCV

--- OpenCV/build/modules/python3/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/maxim/OpenCV/modules/python/python3
-# Build directory: /home/maxim/OpenCV/build/modules/python3
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
